633b5affdd78fd45e82d74cafdd146a247f739b8
ryden47 <guetaaha@post.bgu.ac.il>
2022-04-27 21:20:02 +0300

    adding an option to return only sum [in def neighbors(X)]

--- main.py
+++ main.py
@@ -9,20 +9,26 @@ F = lambda row_s, row_t, temp: \
     np.exp((1/temp) * sum(np.multiply(row_s, row_t)))
 
 
-def neighborsSet(X):  # I prefer returning an Iterator, saving a lot of space. But how..?
-    neighbors = []
+'''I made an option to return the Set of all neighbors, just in case we'll need that in the future.
+    By default, we just return the sum of all Xs*Xt, when Xs Xt are neighbors in X'''
+def neighbors(X, returnSet=False):
+    sum, neighbors = 0, []
     for i in range(0, X.shape[0]):
         for j in range(0, X.shape[1]):
-            neighbors.append([X[i][j], X[i][j+1]]) if j+1 < X.shape[1] else None
-            neighbors.append([X[i][j], X[i+1][j]]) if i+1 < X.shape[0] else None
-    return neighbors
+            if j+1 < X.shape[1]:
+                neighbors.append([X[i][j], X[i][j+1]]) if returnSet else None
+                sum += X[i][j] * X[i][j+1]
+            if i+1 < X.shape[0]:
+                neighbors.append([X[i][j], X[i + 1][j]]) if returnSet else None
+                sum += X[i][j]*X[i+1][j]
+    return neighbors if returnSet else sum
 
 
 def Z_temp(temp, n):
     ret = 0
     for X in product([-1, 1], repeat=n*n):
         X = np.array(X).reshape(n, n)
-        ret += np.exp(1/temp * sum(map(lambda s: s[0]*s[1], neighborsSet(X))))
+        ret += np.exp(1/temp * neighbors(X))
     return ret
 
 
